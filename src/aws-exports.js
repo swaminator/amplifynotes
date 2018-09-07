4c0a56352001c332b642798f515d89d6bf2dcff3
Nikhil Swaminathan <nsswamin@amazon.com>
2018-09-07 09:57:23 -0700

    fixed exports error

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile =  {
     "aws_cognito_region": process.env.aws_cognito_region,
     "aws_user_pools_id": process.env.aws_user_pools_id,
     "aws_user_pools_web_client_id": process.env.aws_user_pools_web_client_id,
-    "aws_appsync_graphqlEndpoint": process.env.aws_appsync_graphqlEndpoint,,
+    "aws_appsync_graphqlEndpoint": process.env.aws_appsync_graphqlEndpoint,
     "aws_appsync_region": process.env.aws_appsync_region,
     "aws_appsync_authenticationType": process.env.aws_appsync_authenticationType
 };
